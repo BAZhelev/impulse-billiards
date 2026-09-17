2453b8e94e99527e0d7b6ca7b2cee869302f29d5
Boyan.Zhelev <boyanz35@gmail.com>
2026-09-17 18:00:38 +0300

    feat: update configs

--- apps/cms/next.config.ts
+++ apps/cms/next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+  outputFileTracingRoot: path.join(__dirname, "../.."),
   images: {
     localPatterns: [
       {
